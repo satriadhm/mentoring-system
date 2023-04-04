6dd179f7e74fe75086e713b48fe7068a9b414313
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-04-04 08:10:11 +0700

    menambahkan my schedule page

--- view/Mentee/DashboardMentee.cs
+++ view/Mentee/DashboardMentee.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using mentoring_system.view.Mentee;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -17,6 +18,8 @@ namespace mentoring_system.view
             InitializeComponent();
             coursePage.SendToBack();
             bookingPage.SendToBack();
+            mySchedulePage.SendToBack();
+            
         }
 
         private void bookButton_Click(object sender, EventArgs e)
@@ -34,7 +37,7 @@ namespace mentoring_system.view
 
         private void scheduleButton_Click(object sender, EventArgs e)
         {
-
+            mySchedulePage.BringToFront();
         }
 
         private void lessonButton_Click(object sender, EventArgs e)
