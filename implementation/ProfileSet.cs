04a90a5a86b67293032fc53d08308b5ebc5ad9ed
LAPTOP-GTQCMDG9\Asus <vidyaratmayanti@gmail.com>
2023-05-04 13:29:01 +0700

    penambahan debug assert profile set

--- implementation/ProfileSet.cs
+++ implementation/ProfileSet.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Runtime.CompilerServices;
 using System.Text;
@@ -22,18 +23,28 @@ namespace mentoring_system.implementation
             this.password = password;
         }
 
-        public void ProfileSets(T namaUser, T umurUser, T userName, T password) { 
+        // Precondition: parameter namaUser, umurUser, userName, dan password tidak boleh null
+        // Postcondition: objek ProfileSet diisi dengan parameter namaUser, umurUser, userName, dan password
+        public void ProfileSets(T namaUser, T umurUser, T userName, T password)
+        {
+            Debug.Assert(namaUser != null, "namaUser tidak boleh null");
+            Debug.Assert(umurUser != null, "umurUser tidak boleh null");
+            Debug.Assert(userName != null, "userName tidak boleh null");
+            Debug.Assert(password != null, "password tidak boleh null");
+
             this.namaUser = namaUser;
             this.umurUser = umurUser;
             this.userName = userName;
             this.password = password;
         }
 
-        public T Getnama() { 
+        public T Getnama()
+        {
             return this.namaUser;
         }
 
-        public T Getumur() {
+        public T Getumur()
+        {
             return this.umurUser;
         }
 
@@ -48,12 +59,18 @@ namespace mentoring_system.implementation
             return this.password;
         }
 
-        public void PrintProfile() {
-  
-            Console.WriteLine("Nama: "+ Getnama() );
+        // Invariant: namaUser, umurUser, userName, dan password tidak boleh null
+        public void PrintProfile()
+        {
+            Debug.Assert(namaUser != null, "namaUser tidak boleh null");
+            Debug.Assert(umurUser != null, "umurUser tidak boleh null");
+            Debug.Assert(userName != null, "userName tidak boleh null");
+            Debug.Assert(password != null, "password tidak boleh null");
+
+            Console.WriteLine("Nama: " + Getnama());
             Console.WriteLine("Umur " + Getumur());
-            Console.WriteLine("Username "+ GetuserName());
-            Console.WriteLine("Password: "+ Getpassword());
+            Console.WriteLine("Username " + GetuserName());
+            Console.WriteLine("Password: " + Getpassword());
         }
     }
 }
